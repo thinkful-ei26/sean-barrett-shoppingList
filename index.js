69e3481514b0d81000e4f6cf12298a446bf90729
carpenter-js <carpenterbarrett@gmail.com>
2018-11-01 13:42:29 -0600

    finished shopping list

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function main() {
   // console.log('DOM finished');
 
   $('#js-shopping-list-form').submit(function(e) {
-    console.log($(e.target));
+    const formInput = $(e.target).find('input').val();
     e.preventDefault();
     $('.shopping-list').append(`
       <li>
-        <span class="shopping-item">apples</span>
+        <span class="shopping-item">${formInput}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
             <span class="button-label">check</span>
@@ -20,9 +20,20 @@ function main() {
       </li>
     `);
 
-    // e.target().find('input').reset();
+    $(e.target).find('input').val('');
     // trying to clear the submission
   });
+
+  $('ul').on('click', '.shopping-item-toggle', function(e) {
+    //console.log($(e.target));
+    $(e.target).closest('button').closest('div').siblings('span').toggleClass('shopping-item__checked');
+  });
+  $('ul').on('click', '.shopping-item-delete', function(e) {
+    $(e.currentTarget).closest('div').closest('li').remove(); 
+  });
 }
+
+
+
 
 $(main());
